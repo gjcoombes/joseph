88a311a413b72fe2e301b397bf93feac2e8d0711
Gavin Coombes <gjcoombes@gmail.com>
2013-08-04 00:04:01 +0800

    add reverse url lookup

--- joseph/urls.py
+++ joseph/urls.py
@@ -1,3 +1,16 @@
+#! /usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Module: urls.py
+Created on Sun Aug 04 00:02:22 2013
+@author: gav
+Description:
+
+"""
+### Imports
+from __future__ import print_function
+from __future__ import absolute_import
+
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
@@ -8,11 +21,24 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'joseph.views.home', name='home'),
     # url(r'^joseph/', include('joseph.foo.urls')),
-    url(r'^winfates/', include('winfates.urls')),
-
+    url(r'^winfates/', include('winfates.urls', namespace="winfates")),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+### Constants
+
+### Classes
+
+### Functions
+
+### Tests
+
+if __name__ == "__main__":
+
+
+    print("Done __main__")
+
--- winfates/views.py
+++ winfates/views.py
@@ -12,10 +12,10 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
-
-from winfates.models import Sighting
+from winfates.models import Sighting, Machine
 ### Constants
 
 ### Classes
@@ -23,14 +23,12 @@ from winfates.models import Sighting
 ### Functions
 def index(request):
     latest_sighting_list = Sighting.objects.order_by('-time_recv')[:3]
-    template = loader.get_template('winfates/index.html')
-    context = RequestContext(request, {
-        "latest_sighting_list": latest_sighting_list,
-        })
-    return HttpResponse(template.render(context))
+    context = {"latest_sighting_list": latest_sighting_list}
+    return render(request, 'winfates/index.html', context)
 
 def machine(request, machine_id):
-    return HttpResponse("You are looking at machine {}".format(machine_id))
+    mach = get_object_or_404(Machine, pk=machine_id)
+    return render(request, 'winfates/machine.html', {'machine': mach})
 
 def sighting(request, sighting_id):
     return HttpResponse("You are looking at sighting {}".format(sighting_id))
